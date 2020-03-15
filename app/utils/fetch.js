4fd4498b67930cebe36d48d64f65bcb8a921a044
Victor Statko <statkovit@gmail.com>
2020-03-15 22:07:50 +0300

    do not refresh token on logout

--- app/utils/fetch.js
+++ app/utils/fetch.js
@@ -33,43 +33,52 @@ export default async (method, path, data, headers) => {
         },
         async function (error) {
             const originalRequest = error.config;
+
             if (error.response && error.response.status === 401) {
                 const refreshToken = localStorageService.getRefreshToken();
-                if (refreshToken) {
-                    try {
-                        const res = await axios.post(`${BACKEND_URL}${'auth/token'}`,
-                            queryString.stringify({
-                                "refresh_token": refreshToken,
-                                "grant_type": "refresh_token",
-                                "client_id": "webclient"
-                            }), {
-                                headers: {"Content-Type": "application/x-www-form-urlencoded"}
-                            });
+                const logoutTry = error.config.url === `${BACKEND_URL}${'auth/logout'}`;
 
-                        if (res.status === 200) {
-                            localStorageService.setToken(
-                                {
-                                    access_token: res.data.access_token,
-                                    refresh_token: res.data.refresh_token
-                                }
-                            );
+                if (logoutTry || !refreshToken) {
+                    localStorageService.clearToken();
+                    await indexedDBService.clearKeys();
+                    store.dispatch(userLoggedOut());
 
-                            originalRequest.headers['Authorization'] = 'Bearer ' + localStorageService.getAccessToken();
-
-                            return axios(originalRequest);
-                        }
-                    } catch (e) {
+                    if (!logoutTry) {
                         toast.error(i18n.t('global.auth.sessionExpired'));
-                        localStorageService.clearToken();
-                        await indexedDBService.clearKeys();
-                        store.dispatch(userLoggedOut());
                     }
-                } else {
+
+                    return;
+                }
+
+                try {
+                    const res = await axios.post(`${BACKEND_URL}${'auth/token'}`,
+                        queryString.stringify({
+                            "refresh_token": refreshToken,
+                            "grant_type": "refresh_token",
+                            "client_id": "webclient"
+                        }), {
+                            headers: {"Content-Type": "application/x-www-form-urlencoded"}
+                        });
+
+                    if (res.status === 200) {
+                        localStorageService.setToken(
+                            {
+                                access_token: res.data.access_token,
+                                refresh_token: res.data.refresh_token
+                            }
+                        );
+
+                        originalRequest.headers['Authorization'] = 'Bearer ' + localStorageService.getAccessToken();
+
+                        return axios(originalRequest);
+                    }
+                } catch (e) {
                     toast.error(i18n.t('global.auth.sessionExpired'));
                     localStorageService.clearToken();
                     await indexedDBService.clearKeys();
                     store.dispatch(userLoggedOut());
                 }
+
             } else {
                 return Promise.reject(error.response)
             }
